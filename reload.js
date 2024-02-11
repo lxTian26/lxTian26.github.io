ce2d06ad66de0fd47b8c2fc6da9a13625fe61fd5
Lingxuan-Tian <149690210+lxTian26@users.noreply.github.com>
2024-02-11 18:50:42 +0000

    wertr

--- reload.js
+++ reload.js
@@ -1,27 +1,14 @@
-function reloadPage() {
+(function()
+ {
+ if( window.localStorage )
+ {
+ if( !localStorage.getItem( 'firstLoad' ) )
+ {
+ localStorage[ 'firstLoad' ] = true;
+location.reload(true);
+ } 
 
-    // The last "domLoading" Time //
-    
-    var currentDocumentTimestamp =
-    
-    new Date(performance.timing.domLoading).getTime();
-    
-    // Current Time //
-    
-    var now = Date.now();
-    
-    // Ten Seconds //
-    
-    var tenSec = 10 * 1000;
-    
-    // Plus Ten Seconds //
-    
-    var plusTenSec = currentDocumentTimestamp + tenSec;
-    
-    if (now > plusTenSec) {
-    
-    location.reload();
-    
-    } else {}
-    
-    }
+ else
+ localStorage.removeItem( 'firstLoad' );
+ }
+ })();
